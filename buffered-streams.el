f4feea262e36b4b110bd05bd8fd3435efa5754c9
Isaac Lewis <isaac.b.lewis@gmail.com>
2018-07-05 21:52:45 -0400

    Add buffered-sockets.el

---
+++ buffered-streams.el
@@ -0,0 +1,33 @@
+;;; buffered-streams.el --- Buffered streams for emacs -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Free Software Foundation, Inc.
+
+;; Author: Isaac Lewis <isaac.b.lewis@gmail.com>
+;; Version: 1.0.0
+;; Keywords: buffered, stream
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Buffered stream for Emacs
+;;
+;; See the documentation at https://github.com/IkeLewis/buffered-streams
+;;
+
+;;; Code:
+
+(provide 'buffered-streams)
+;;; buffered-streams.el ends here
