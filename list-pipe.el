f7417b2178af22039d3b9e118d4595362068dc73
Isaac Lewis <isaac.b.lewis@gmail.com>
2018-07-12 19:45:00 -0400

    Add list-pipe.el

---
+++ list-pipe.el
@@ -0,0 +1,30 @@
+;;; list-pipe.el --- List-Pipes for Emacs -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Isaac Lewis
+
+;; Author: Isaac Lewis <isaac.b.lewis@gmail.com>
+;; Version: 1.0.0
+;; Keywords: comm
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; TODO
+
+;;{{{
+;; Code:
+
+(provide 'list-pipe)
+;;; list-pipe.el ends here
