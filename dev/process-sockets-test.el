47e0643104ca48d062203bfc51e6ec14081ce079
Isaac Lewis <isaac.b.lewis@gmail.com>
2018-07-14 23:57:41 -0400

    Add the license and copyright info to dev/process-sockets-test.el.

---
+++ dev/process-sockets-test.el
@@ -0,0 +1,27 @@
+;;; process-sockets-test.el --- Tests for process sockets -*- lexical-binding: t -*-
+
+;; Copyright (C) 2018 Isaac Lewis
+
+;; Author: Isaac Lewis <isaac.b.lewis@gmail.com>
+;; Version: 1.0.0
+;; Keywords: comm
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; TODO
+
+(provide' process-sockets-test)
+;; process-sockets-test.el ends here
